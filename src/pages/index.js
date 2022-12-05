535ee3e8ddc6592dc30e5401644d6553d4d3bfb1
Leon <leonchike@gmail.com>
2022-12-05 17:19:40 -0500

    added theme ui styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+// pages/index.jsx
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Link from "next/link";
 
-const Page = () => {
-  return (
-    <div>
-      <h1>Index</h1>
-
-      <div>
-        <Link href="/notes">To Notes</Link>
-      </div>
+export default () => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: "containers.page",
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>
+        This is a really dope note taking app.
+      </h1>
     </div>
-  );
-};
-
-export default Page;
+  </div>
+);
